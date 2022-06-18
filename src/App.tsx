aa9f3a0c6962ea00f672b12f16ff356f0decf56e
lena zamnius <e.zamnius@evnedev.com>
2022-06-18 13:41:22 +0200

    basic project setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import {Layout} from "antd";
+
+const {  Content } = Layout;
+
+const App: React.FC = () => {
+  return (
+    <Layout>
+      <Content>Content</Content>
+    </Layout>
+  );
+}
+
+export default App;
